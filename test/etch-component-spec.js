be9bdd0cac1a47c869b92056c5543f38d1ce5f1c
Allen Smith <loranallensmith@github.com>
2016-09-11 21:11:10 -0400

    update component test

--- test/etch-component-spec.js
+++ test/etch-component-spec.js
@@ -12,11 +12,12 @@ import EtchComponent from '../src/etch-component';
 
 describe('EtchComponent', () => {
 
-  describe('new EtchComponent()', () => {
+  describe('constructor', () => {
 
     it('should not be instantiable', () => {
       const fn = () => {
         let myComponent = new EtchComponent({ type: 'abstract' }, []);
+        return myComponent;
       };
       expect(fn).to.throw(TypeError);
     });
