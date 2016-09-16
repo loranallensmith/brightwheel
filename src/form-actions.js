c78a6c70706c7248e0df6479c5e3d5b39967db99
Allen Smith <loranallensmith@github.com>
2016-09-15 23:13:10 -0400

    Add FormActions

---
+++ src/form-actions.js
@@ -0,0 +1,33 @@
+/**
+ * breit-wheeler
+ *
+ * Copyright © 2016 Allen Smith &lt;loranallensmith@github.com&gt;. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+ /** @jsx etch.dom */
+
+import etch from 'etch';
+import classNames from 'classnames';
+import EtchComponent from './etch-component';
+
+class FormActions extends EtchComponent {
+
+  render() {
+    let classes = classNames(
+      'form-actions',
+      this.properties.classNames
+    );
+    return (
+      <div className={classes}>
+        {this.children.map(function(child) {
+          child
+        })}
+      </div>);
+  }
+
+}
+
+export default FormActions;
---
+++ test/form-actions-spec.js
@@ -0,0 +1,41 @@
+/**
+ * breit-wheeler
+ *
+ * Copyright © 2016 Allen Smith &lt;loranallensmith@github.com&gt;. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import { expect } from 'chai';
+import FormActions from '../src/form-actions';
+import Button from '../src/button';
+
+describe('FormActions', () => {
+
+  describe('render', () => {
+
+    it('should create the right element', () => {
+      let myActions = new FormActions({}, []);
+      expect(myActions.virtualElement.tagName).to.equal('DIV');
+    });
+
+    it('should render the correct class', () => {
+      let myActions = new FormActions({}, []);
+      expect(myActions.virtualElement.properties.className).to.include('form-actions');
+    });
+
+    it('should include extra classes', () => {
+      let myActions = new FormActions({ text: 'My FormActions', classNames:['extra-class', 'another-class']}, []);
+      expect(myActions.virtualElement.properties.className).to.contain('extra-class another-class');
+    });
+
+    it('should render children elements', () => {
+      let myButton = new Button({ text: 'Submit' }, [])
+      let myActions = new FormActions({}, [myButton]);
+      expect(myActions.children[0].constructor.name).to.equal('Button');
+    });
+
+  });
+
+});
