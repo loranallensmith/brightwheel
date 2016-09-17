84d22c7754a76661641f070ee4e08abbe6c721c2
Allen Smith <loranallensmith@github.com>
2016-09-16 22:37:26 -0400

    Add ButtonGroup

---
+++ test/button-group-spec.js
@@ -0,0 +1,41 @@
+/**
+ * breit-wheeler
+ *
+ * Copyright © 2016 Allen Smith &lt;loranallensmith@github.com&gt;. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import { expect } from 'chai';
+import ButtonGroup from '../src/button-group';
+import Button from '../src/button';
+
+describe('ButtonGroup', () => {
+
+  describe('render method', () => {
+
+    it('should create the right element', () => {
+      let myButtonGroup = new ButtonGroup({}, []);
+      expect(myButtonGroup.virtualElement.tagName).to.equal('DIV');
+    });
+
+    it('should render the correct class', () => {
+      let myButtonGroup = new ButtonGroup({}, []);
+      expect(myButtonGroup.virtualElement.properties.className).to.include('btn-group');
+    });
+
+    it('should include extra classes', () => {
+      let myButtonGroup = new ButtonGroup({ classNames:['extra-class', 'another-class'] }, []);
+      expect(myButtonGroup.virtualElement.properties.className).to.contain('extra-class another-class');
+    });
+
+    it('should render children', () => {
+      let myButton = new Button({ text: 'My Button' }, [])
+      let myButtonGroup = new ButtonGroup({}, [myButton]);
+      expect(myButtonGroup.children[0].constructor.name).to.equal('Button');
+    });
+
+  });
+
+});
