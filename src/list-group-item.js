407626c859cc04e93579a30d56da6360277f1607
Allen Smith <loranallensmith@github.com>
2016-09-16 20:18:50 -0400

    Add ListGroupItem

---
+++ src/list-group-item.js
@@ -0,0 +1,42 @@
+/**
+ * breit-wheeler
+ *
+ * Copyright © 2016 Allen Smith &lt;loranallensmith@github.com&gt;. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+ /** @jsx etch.dom */
+
+import etch from 'etch';
+import classNames from 'classnames';
+import EtchComponent from './etch-component';
+import Icon from './icon';
+
+class ListGroupItem extends EtchComponent {
+
+  render() {
+
+    let itemType = this.properties.header === true ? 'header' : 'item';
+
+    let classes = classNames(
+      [`list-group-${itemType}`],
+      {
+        'active': this.properties.active === true
+      },
+      this.properties.classNames,
+    );
+
+    return (
+      <li className={classes}>
+        {this.children.map(function(child) {
+          {child}
+        })}
+      </li>
+    );
+  }
+
+}
+
+export default ListGroupItem;
---
+++ test/list-group-item-spec.js
@@ -0,0 +1,44 @@
+/**
+ * breit-wheeler
+ *
+ * Copyright © 2016 Allen Smith &lt;loranallensmith@github.com&gt;. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import { expect } from 'chai';
+import ListGroupItem from '../src/list-group-item';
+import Image from '../src/image';
+import MediaBody from '../src/media-body';
+
+describe('ListGroupItem', () => {
+
+  describe('render method', () => {
+
+    it('should render the correct element', () => {
+      let myItem = new ListGroupItem({}, []);
+      expect(myItem.virtualElement.tagName).to.equal('LI');
+    });
+
+    it('should conditionally render as active', () => {
+      let myItem = new ListGroupItem({ active: true }, []);
+      expect(myItem.virtualElement.properties.className).to.contain('active');
+    });
+
+    it('should include extra classes', () => {
+      let myItem = new ListGroupItem({ classNames: ['custom-class', 'another-class'] }, []);
+      expect(myItem.virtualElement.properties.className).to.contain('custom-class');
+    });
+
+    it('should render children', () => {
+      let myImage = new Image({ src: 'file.jpg', pull: 'left'}, []);
+      let myBody = new MediaBody({ title: 'My body', text: 'My text'}, []);
+      let myItem = new ListGroupItem({}, [myImage, myBody]);
+      expect(myItem.children[0].virtualElement.tagName).to.equal('IMG');
+      expect(myItem.children[1].virtualElement.tagName).to.equal('DIV');
+    });
+
+  });
+
+});
