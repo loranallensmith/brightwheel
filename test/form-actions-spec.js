f6d2e1a53227aa63fcf88d941a9d34461d7545b8
Allen Smith <loranallensmith@github.com>
2017-08-01 09:39:31 -0400

    Fix form-actions tests

--- test/form-actions-spec.js
+++ test/form-actions-spec.js
@@ -17,23 +17,23 @@ describe('FormActions', () => {
 
     it('should create the right element', () => {
       let myActions = new FormActions({}, []);
-      expect(myActions.virtualElement.tagName).to.equal('DIV');
+      expect(myActions.element.tagName).to.equal('DIV');
     });
 
     it('should render the correct class', () => {
       let myActions = new FormActions({}, []);
-      expect(myActions.virtualElement.properties.className).to.include('form-actions');
+      expect(myActions.element.className).to.include('form-actions');
     });
 
     it('should include extra classes', () => {
       let myActions = new FormActions({ text: 'My FormActions', classNames:['extra-class', 'another-class']}, []);
-      expect(myActions.virtualElement.properties.className).to.contain('extra-class another-class');
+      expect(myActions.element.className).to.contain('extra-class another-class');
     });
 
     it('should render children elements', () => {
       let myButton = new Button({ text: 'Submit' }, [])
       let myActions = new FormActions({}, [myButton]);
-      expect(myActions.children[0].constructor.name).to.equal('Button');
+      expect(myActions.element.children[0].tagName).to.equal('BUTTON');
     });
 
   });
