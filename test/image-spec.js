243daa53b18f9896b65a4cb95d0c7e01d85f7f86
Allen Smith <loranallensmith@github.com>
2017-08-01 09:36:28 -0400

    Fix image tests  Note: The test for rendering an image source is still a bit weak since it only checks for the specified path somewhere in the src attribute. This is to account for the way electron-mocha renders files for the tests.  A future improvement would account for this to ensure the src attribute is exactly as specified.

--- test/image-spec.js
+++ test/image-spec.js
@@ -16,32 +16,32 @@ describe('Image', () => {
 
     it('should render the correct tag', () => {
       let myImage = new Image({}, []);
-      expect(myImage.virtualElement.tagName).to.equal('IMG');
+      expect(myImage.element.tagName).to.equal('IMG');
     });
 
     it('Should render the source', () => {
       let myImage = new Image({ src: 'images/test.jpg' }, []);
-      expect(myImage.virtualElement.properties).to.contain.keys('src': 'images/test.jpg');
+      expect(myImage.element.src).to.include('images/test.jpg');
      });
 
     it('Should conditionally render the width', () => {
      let myWideImage = new Image({ width: 32 }, []);
-     expect(myWideImage.virtualElement.properties).to.contain.keys('width': 32);
+     expect(myWideImage.element.width).to.equal(32);
    });
 
     it('Should conditionally render the height', () => {
       let myTallImage = new Image({ height: 32 }, []);
-      expect(myTallImage.virtualElement.properties).to.contain.keys('height': 32);
+      expect(myTallImage.element.height).to.equal(32);
     });
 
     it('Should conditionally pull in the correct direction', () => {
       let myPulledImage = new Image({ pull: 'right' }, []);
-      expect(myPulledImage.virtualElement.properties.className).to.contain('pull-right');
+      expect(myPulledImage.element.className).to.contain('pull-right');
     });
 
     it('Should render as media object when specified', () => {
       let myMediaImage = new Image({ isMediaObject: true }, []);
-      expect(myMediaImage.virtualElement.properties.className).to.contain('media-object');
+      expect(myMediaImage.element.className).to.contain('media-object');
     });
 
   });
