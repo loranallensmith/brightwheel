fb912a9745585350ecf1a354eed11342ddee804c
Allen Smith <loranallensmith@github.com>
2017-08-01 09:42:30 -0400

    Fix checkbox tests

--- test/checkbox-spec.js
+++ test/checkbox-spec.js
@@ -16,32 +16,32 @@ describe('CheckBox', () => {
 
     it('Should render the correct elements', () => {
       let myCheckbox = new CheckBox({}, []);
-      let firstChild = myCheckbox.virtualElement.children[0];
-      expect(myCheckbox.virtualElement.tagName).to.equal('DIV');
-      expect(firstChild.constructor.name).to.equal('ComponentWidget');
+      let firstChild = myCheckbox.element.children[0];
+      expect(myCheckbox.element.tagName).to.equal('DIV');
+      expect(firstChild.tagName).to.equal('LABEL');
       expect(firstChild.children[0].tagName).to.equal('INPUT');
     });
 
     it('Should have the correct default classes', () => {
       let myCheckbox = new CheckBox({}, []);
-      expect(myCheckbox.virtualElement.properties.className).to.include('checkbox');
+      expect(myCheckbox.element.className).to.include('checkbox');
     });
 
     it('Should support multiple classes', () => {
       let myCheckbox = new CheckBox({ classNames: ['extra-class', 'another-class'] }, []);
-      expect(myCheckbox.virtualElement.properties.className).to.include('another-class');
-      expect(myCheckbox.virtualElement.properties.className).to.include('extra-class');
+      expect(myCheckbox.element.className).to.include('another-class');
+      expect(myCheckbox.element.className).to.include('extra-class');
     });
 
     it('Should render a name', () => {
       let myCheckbox = new CheckBox({ name: 'my-radio'}, []);
-      let label = myCheckbox.virtualElement.children[0];
-      expect(label.children[0].properties).to.contain.keys({ name: 'my-radio' });
+      let label = myCheckbox.element.children[0];
+      expect(label.children[0].name).to.equal('my-radio');
     });
 
     it('Should render the label text', () => {
       let myCheckbox = new CheckBox({ text: 'Text content'}, []);
-      expect(myCheckbox.virtualElement.children[0].component.properties).to.contain.keys({ text: 'Text content' });
+      expect(myCheckbox.element.children[0].textContent).to.equal('Text content');
     });
 
   });
